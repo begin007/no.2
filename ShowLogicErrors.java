6e215e0b0223c706b4355fb1fcfc7614dd4755e6
begin007 <54835118+begin007@users.noreply.github.com>
2019-09-10 16:35:30 +0800

    Add files via upload

---
+++ ShowLogicErrors.java
@@ -0,0 +1,10 @@
+package jxz;
+
+public class ShowLogicErrors
+{
+	public static void main(String[] args)
+	{
+	      System.out.print("Celsius 35 is Fahrenheit degree");
+	      System.out.print((9 /5) * 35 + 32);
+	}
+}
