47d3a152e957b2476f6b8c15713b1d68b0619666
begin007 <54835118+begin007@users.noreply.github.com>
2019-09-10 15:35:23 +0800

    Add files via upload

---
+++ hhh.java
@@ -0,0 +1,12 @@
+package jxz;
+
+public class hhh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int num=8;
+      System.out.print("The value is:"+'\n'+num/2+'\n');
+      System.out.print("The following is:\n"+
+    		          5*6+(6-2)+6+2);
+	}
+}
