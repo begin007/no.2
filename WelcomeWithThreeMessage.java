0d6bbf66ea189199e91490b38c8952fdd1a930c9
begin007 <54835118+begin007@users.noreply.github.com>
2019-09-10 16:10:02 +0800

    Add files via upload

---
+++ WelcomeWithThreeMessage.java
@@ -0,0 +1,11 @@
+package jxz;
+
+public class WelcomeWithThreeMessage
+{
+	public static void main(String[] args)
+	{
+		System.out.printf("Programming is fun!\n");
+		System.out.printf("Fundametals First\n");
+		System.out.printf("Problem Driven\n");
+	}
+}
