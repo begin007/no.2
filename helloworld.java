1bff60a07eed3394553edb7f37f0fc747bf4fdd4
begin007 <54835118+begin007@users.noreply.github.com>
2019-09-13 15:10:01 +0800

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+package jxz.jzva;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.printf("slj is sb");
+       
+	}
+
+}
