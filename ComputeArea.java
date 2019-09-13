ae04929ecca0150562438a485cda54b2deec4aad
begin007 <54835118+begin007@users.noreply.github.com>
2019-09-13 13:00:38 +0800

    Add files via upload

---
+++ ComputeArea.java
@@ -0,0 +1,15 @@
+package jxz.jzva;
+
+public class ComputeArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        double radius;
+        double area;
+        radius=20;
+        area=radius*radius*3.14159;
+        System.out.printf("the area for the circle of radius"+radius+"is"+area);
+        		
+	}
+
+}
