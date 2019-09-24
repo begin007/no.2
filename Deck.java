2054a6e19fc2c12e2cd2ef0504e3ea96d8e72fd9
begin007 <54835118+begin007@users.noreply.github.com>
2019-09-24 15:45:19 +0800

    Add files via upload

---
+++ Deck.java
@@ -0,0 +1,14 @@
+package jxz;
+
+public class Deck {
+
+	public Deck() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
