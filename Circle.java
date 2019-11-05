421d9ad5972c3a38e1911b86607be8cc5b71c5b9
begin007 <54835118+begin007@users.noreply.github.com>
2019-11-05 15:31:46 +0800

    Add files via upload

--- Circle.java
+++ Circle.java
@@ -1,14 +1,8 @@
-package jxz;
 
 public class Circle {
 
 	public Circle() {
 		// TODO Auto-generated constructor stub
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
