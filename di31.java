00c03a78cc82dde87b5be9422e27ebca1917cb75
begin007 <54835118+begin007@users.noreply.github.com>
2019-11-26 16:15:56 +0800

    Add files via upload

---
+++ di31.java
@@ -0,0 +1,13 @@
+
+public class di31 {
+
+	public di31() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
